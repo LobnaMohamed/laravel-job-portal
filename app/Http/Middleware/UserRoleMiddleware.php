671b043115216359be033d914ad70f1ad25b9c20
Lobna Mahmoud <lobna@AMOC.COM>
2024-02-29 14:24:39 +0200

    first commit

---
+++ app/Http/Middleware/UserRoleMiddleware.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use function Laravel\Prompts\error;
+
+class UserRoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next,$role): Response
+    {
+        dd($role);
+        if($request->user()->role !== $role){
+            if($request->user()->role ==='company'){
+                return redirect()->route('company.dashboard');
+            }elseif($request->user()->role === 'candidate'){
+                return redirect()->route('dashboard');
+
+            }
+        }
+        return $next($request);
+    }
+}
