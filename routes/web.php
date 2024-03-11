3b97c5c739289452942e67cf06b1966f1c582c21
Lobna Mahmoud <lobna@AMOC.COM>
2024-03-11 13:54:14 +0200

    dashboards done

--- routes/web.php
+++ routes/web.php
@@ -18,28 +18,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified','user.role:candidate'])->name('dashboard');
-
-Route::get('company/dashboard', function () {
-    return view('frontend.company-dashboard.dashboard');
-})->middleware(['auth', 'verified', 'user.role:company'])->name('company.dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+// candidate routes
 Route::group(
     ['middleware'=>['auth', 'verified', 'user.role:candidate'],
     'prefix'=> 'candidate',
-    'as'=>'candidate'], //route name
+    'as'=>'candidate.'], //route name
     function(){
-    Route::get('company/dashboard', function () {
-        return view('dashboard')->name('candidate.dashboard');
-    });
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+});
+// company routes
+Route::group(
+    ['middleware'=>['auth', 'verified', 'user.role:company'],
+    'prefix'=> 'company',
+    'as'=>'company.'], //route name
+    function(){
+    Route::get('/dashboard', function () {
+        return view('frontend.company-dashboard.dashboard');
+    })->name('dashboard');
 
 });
 require __DIR__.'/auth.php';
